3691251e7025dc9e38c356b0cbdbfb138feef006
ridwan rmd <ridwan95rmd@gmail.com>
2023-02-11 20:03:29 +0700

    create new next app pokedex, install chakra

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>Pokedex</title>
+        <meta
+          name="description"
+          content="Most reliable informnation about pokemon"
+        />
+        <link rel="Pokedex icon" href="/pokedex.png" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
